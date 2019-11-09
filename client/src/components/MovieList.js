990fba3dd40c58566e782baedda57d344a3e5cb4
haavarhu <havard.hunshamar@ntnu.no>
2019-11-09 21:42:06 +0100

    Added searchbar

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Movie from "./Movie.js";
 import { Grid } from "@material-ui/core";
+import InfiniteScroll from "react-infinite-scroller";
 
 class MovieList extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class MovieList extends Component {
 
   render() {
     return (
-      <div style={{ width: "80%" }}>
+      <div style={{ width: "80%", margin: "20px" }}>
         <Grid container spacing={1}>
           {this.props.movies.map((movie, i) => {
             return (
--- client/src/reducers/movieListReducer.js
+++ client/src/reducers/movieListReducer.js
@@ -1,13 +1,23 @@
 const initialState = {
-  movieList: []
+  movieList: [],
+  searchSuggestions: [],
+  numberOfPages: 0
 };
 
 export default function movieListReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_MOVIES":
+      console.log(action.movieList.results);
+
       return {
         ...state,
-        movieList: action.payload
+        movieList: action.movieList.results,
+        numberOfPages: action.movieList.total_pages
+      };
+    case "SET_SEARCH_SUGGESTION":
+      return {
+        ...state,
+        searchSuggestions: action.payload
       };
     default:
       return state;
